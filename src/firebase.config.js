32e77246ac8705b7c604f81ddea1b4f6fe8a5128
Shorya Dubey <98734613+shorya1853@users.noreply.github.com>
2024-01-10 17:11:31 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,15 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAeq2qZsQ7VAN9592BpHSWytJD9er4FbUI",
-  authDomain: "web-app-test-aed01.firebaseapp.com",
-  projectId: "web-app-test-aed01",
-  storageBucket: "web-app-test-aed01.appspot.com",
-  messagingSenderId: "601816964471",
-  appId: "1:601816964471:web:1c746cbaa65e996df51572",
-  measurementId: "G-2DHYSK06B8"
-};
+
 
 const app = initializeApp(config);
 
