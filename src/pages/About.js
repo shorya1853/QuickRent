1af86e76142e3df3732fd4a2500f4aef2c2f2efe
shorya <shoryadubey136@gmail.com>
2024-01-11 16:24:32 +0530

    pages created

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+ 
+const About = () => {
+    return (
+        <div>
+            <h1>Contact Us Page</h1>
+        </div>
+    );
+};
+ 
+export default About;
---
+++ src/pages/Sproduct.js
@@ -0,0 +1,11 @@
+import React from "react";
+ 
+const Sproduct = () => {
+    return (
+        <div>
+            <h1>Contact Us Page</h1>
+        </div>
+    );
+};
+ 
+export default Sproduct;
