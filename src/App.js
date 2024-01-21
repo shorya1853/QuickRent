3212381a055e66852c06ea3386846abac233a8f5
unknown <nikhilchetry942@gmail.com>
2024-01-21 15:17:23 +0530

    newupdate

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ function App() {
         </UserContextProvider>
       </ShopContextProvider>
     </div>
+    
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,7 +36,7 @@ function Navbarlog() {
 
   return (
     <>
-      <Navbar key="xxl" expand="xxl" className="bg-body-tertiary mb-3">
+     <Navbar key="xxl" expand="xxl" className="bg-body-tertiary mb-3">
         <Container fluid>
           <Navbar.Brand href="/">Quick Rent</Navbar.Brand>
           <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-xxl`} />
@@ -95,7 +95,30 @@ function Navbarlog() {
           </Navbar.Offcanvas>
         </Container>
       </Navbar>
+     <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="https://images.unsplash.com/photo-1532716377393-5ffbea548d05?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100" width="800" height="600" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="https://images.unsplash.com/photo-1598704710590-dbb8d9815b32?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100"width="800" height="600" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="https://images.unsplash.com/photo-1554941426-a965fb2b9258?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100" width="800" height="600" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
     </>
+  
+    
   );
 }
 
